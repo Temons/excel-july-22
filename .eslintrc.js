ab00424ba2452c9df400b2a867c8cabce8503bcc
EXT02D25621 <artsiom.trafimiuk@consultant.turkcell.com.tr>
2023-03-31 13:35:16 +0100

    Add eslint rules, netlify.toml

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,25 @@ module.exports = {
     },
   },
   env: {
-    browser: true,
-    node: true,
-    es6: true,
+    "browser": true,
+    "node": true,
+    "es6": true,
     "jest/globals": true
   },
   extends: ["eslint:recommended", "google"],
   plugins: ["jest"],
   rules: {
-    semi: "off",
+    "semi": "off",
     "linebreak-style": "off",
     "comma-dangle": "off",
-    quotes: 0,
+    "quotes": 0,
     "require-jsdoc": "off",
     "operator-linebreak": "off",
+    "react/jsx-indent": [2, 2],
+    "indent": [2, 2],
+    "react/react-in-jsx-scope": "off",
+    "object-curly-spacing": ["error", "always"],
+    "no-undef": "off",
+    "react/display-name": "off"
   },
 };
